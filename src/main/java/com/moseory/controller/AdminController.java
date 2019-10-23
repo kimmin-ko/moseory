4206fbb7494c842d0cd1a97e17761a5acd525fbd
ehgud <ehgud1008@naver.com>
2019-10-23 20:24:35 +0900

    VO,CONTROLLER

---
+++ src/main/java/com/moseory/controller/AdminController.java
@@ -0,0 +1,93 @@
+package com.moseory.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.moseory.domain.ProductDetailVO;
+import com.moseory.domain.ProductVO;
+//import com.moseory.service.AdminServiceImpl;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+//	@Autowired
+//	private AdminServiceImpl adminService;
+	
+	@GetMapping("/productregist")
+	public String productRegist() {
+		
+		return "admin/productregist";
+	}
+
+	@PostMapping("/productregist")
+	public String productRegist(
+			@ModelAttribute ProductVO productVO,
+			@ModelAttribute ProductDetailVO productdetailVO
+			) {
+		System.out.println(productVO);
+		System.out.println(productdetailVO);
+//		adminService.regist(productVO, productdetailVO);
+		
+		return "redirect:/home";
+	}
+		
+	@RequestMapping(value="/imageUpload", method=RequestMethod.POST)
+	public void imageUpload(HttpServletRequest request, HttpServletResponse response,
+								@RequestParam(value="upload") MultipartFile upload) {
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
+        
+        OutputStream out = null;
+        PrintWriter printWriter = null;
+        
+        try{
+        	String fileName = upload.getOriginalFilename();
+        	byte[] bytes = upload.getBytes();
+        	String uploadPath = request.getSession().getServletContext().getRealPath("/")+"/admin/imageUpload/"+fileName;
+        	out = new FileOutputStream(new File(uploadPath));
+        	out.write(bytes);
+        	String callback = request.getParameter("CKEditorFuncNum");
+        	
+        	printWriter = response.getWriter();
+        	String fileUrl = request.getContextPath() + "/admin/imageUpload/"+fileName;
+        	
+        	printWriter.println("<script type='text/javascript'>window.parent.CKEDITOR.tools.callFunction("
+                    + callback
+                    + ",'"
+                    + fileUrl
+                    + "','이미지를 업로드 하였습니다.'"
+                    + ")</script>");
+        	
+        	printWriter.flush();
+        }catch(IOException e){
+        	e.printStackTrace();
+        }finally{
+        	try{
+        		if(out != null) out.close();
+        		if(printWriter != null) printWriter.close();
+        	}catch(IOException e){
+        		e.printStackTrace();
+        	}
+        }
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/com/moseory/domain/ProductDetailVO.java
@@ -0,0 +1,11 @@
+package com.moseory.domain;
+
+import lombok.Data;
+
+@Data
+public class ProductDetailVO {
+
+	private int no;
+	private String product_code, product_color, product_size, product_stock;
+	
+}
