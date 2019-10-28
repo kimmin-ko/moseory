215c3d2fafe2d532df2ae6ecb0ad7ea364706644
kimmin <kimmin2301@gmail.com>
2019-10-28 21:40:27 +0900

    .

--- src/main/java/com/moseory/service/AdminService.java
+++ src/main/java/com/moseory/service/AdminService.java
@@ -1,11 +1,8 @@
 package com.moseory.service;
 
-import org.springframework.stereotype.Service;
-
 import com.moseory.domain.ProductDetailVO;
 import com.moseory.domain.ProductVO;
 
-@Service
 public interface AdminService {
 
 	void regist(ProductVO productVO, ProductDetailVO productdetailVO);
