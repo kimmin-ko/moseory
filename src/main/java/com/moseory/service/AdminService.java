4dff4703ced40ea2da6097e61c2a1a4eaa81b194
hong8159 <hogu8159@naver.com>
2019-11-05 16:13:45 +0900

    11/05 pull

---
+++ src/main/java/com/moseory/service/AdminService.java
@@ -0,0 +1,24 @@
+
+  package com.moseory.service;
+  
+  import org.springframework.stereotype.Service;
+  
+  import com.moseory.domain.ProductDetailVO; 
+  import com.moseory.domain.ProductVO;
+  
+ 
+import java.util.List;
+
+import com.moseory.domain.ProductDetailVO;
+import com.moseory.domain.ProductVO;
+
+public interface AdminService {
+
+	void product_regist(ProductVO productVO);
+
+	int setCode(String name);
+
+	void product_detail_regist(ProductDetailVO productdetailVO);
+
+
+}
