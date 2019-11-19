d8393fcfec84c6160056b2539a59b6bd0ea8709a
msc <moonseungchan4917@gmail.com>
2019-11-19 16:24:16 +0900

    충돌 수정

--- src/main/java/com/moseory/dao/AdminDaoImpl.java
+++ src/main/java/com/moseory/dao/AdminDaoImpl.java
@@ -19,76 +19,78 @@ import com.moseory.domain.ProductVO;
 @Repository("adminDao")
 public class AdminDaoImpl implements AdminDao{
 
-	@Autowired
-	private SqlSession sqlSession;
-	
-	@Override
-	public void product_regist(ProductVO productVO) {
-		sqlSession.insert("product.regist", productVO);
-	}
-	@Override
-	public int setCode(String name) {
-		return sqlSession.selectOne("product.setCode", name);
-	}
-	@Override
-	public void product_detail_regist(ProductDetailVO productdetailVO) {
-		sqlSession.insert("product.product_detail_regist", productdetailVO);
-	}
-	@Override
-	public String getHighCate(int high_code) {
-		return sqlSession.selectOne("product.getHighCate", high_code);
-	}
-	@Override
-	public String getLowCate(int low_code) {
-		return sqlSession.selectOne("product.getLowCate", low_code);
-	}
-	@Override
-	public List<HighCateVO> getPrantCategory() {
-		return sqlSession.selectList("AdminMapper.getPrantCategory");
-	}
-	@Override
-	public void saveParentsCategory(HighCateVO vo) {
-		sqlSession.insert("AdminMapper.saveParentsCategory", vo);
-	}
-	@Override
-	public int deleteParentsCategory(@Param(value = "codes") ArrayList<Integer> codes) {
-		return sqlSession.delete("AdminMapper.deleteParentsCategory", codes);
-	}
-	@Override
-	public List<LowCateVO> getChildCategory(int highCode) {
-		return sqlSession.selectList("AdminMapper.getChildCategory", highCode);
-	}
-	@Override
-	public void saveChildCategory(LowCateVO vo) {
-		sqlSession.insert("AdminMapper.saveChildCategory", vo);		
-	}
-	@Override
-	public int deleteChildCategory(@Param(value = "codes") ArrayList<Integer> codes) {
-		return sqlSession.delete("AdminMapper.deleteChildCategory", codes);
-	}
-	@Override
-	public List<ProductVO> getProductList(int start, int finish) {
-		Map<String, Integer> param = Map.of("start", start, "finish", finish);
-		//1 페이지 = 1~ 10
-		//2 페이지 = 11~20
-		//3 페이지 = 21~30
-		//~~
-		System.out.println(start + "" +finish);
-		Map<String, Integer> param = new HashMap<String, Integer>();
-		param.put("start", start);
-		param.put("finish", finish);
-		System.out.println(sqlSession.selectList("product.getProductList",param));
-		return sqlSession.selectList("product.getProductList",param);
-	}
-	@Override
-	public int getProductCount() {
-		return sqlSession.selectOne("product.getProductCount");
-	}
-	@Override
-	public List<ProductVO> getProductList(int start, int finish, String searchType, String keyword) {
-		Map<String, Object> param = Map.of("start", start, "finish", finish, "searchType", searchType, "keyword", keyword);
-
-		return sqlSession.selectList("product.getListOnSearch", param);
-	}
-
+   @Autowired
+   private SqlSession sqlSession;
+   
+   @Override
+   public void product_regist(ProductVO productVO) {
+      sqlSession.insert("product.regist", productVO);
+   }
+   @Override
+   public int setCode(String name) {
+      return sqlSession.selectOne("product.setCode", name);
+   }
+   @Override
+   public void product_detail_regist(ProductDetailVO productdetailVO) {
+      sqlSession.insert("product.product_detail_regist", productdetailVO);
+   }
+   @Override
+   public String getHighCate(int high_code) {
+      return sqlSession.selectOne("product.getHighCate", high_code);
+   }
+   @Override
+   public String getLowCate(int low_code) {
+      return sqlSession.selectOne("product.getLowCate", low_code);
+   }
+   @Override
+   public List<HighCateVO> getPrantCategory() {
+      return sqlSession.selectList("AdminMapper.getPrantCategory");
+   }
+   @Override
+   public void saveParentsCategory(HighCateVO vo) {
+      sqlSession.insert("AdminMapper.saveParentsCategory", vo);
+   }
+   @Override
+   public int deleteParentsCategory(@Param(value = "codes") ArrayList<Integer> codes) {
+      return sqlSession.delete("AdminMapper.deleteParentsCategory", codes);
+   }
+   @Override
+   public List<LowCateVO> getChildCategory(int highCode) {
+      return sqlSession.selectList("AdminMapper.getChildCategory", highCode);
+   }
+   @Override
+   public void saveChildCategory(LowCateVO vo) {
+      sqlSession.insert("AdminMapper.saveChildCategory", vo);      
+   }
+   @Override
+   public int deleteChildCategory(@Param(value = "codes") ArrayList<Integer> codes) {
+      return sqlSession.delete("AdminMapper.deleteChildCategory", codes);
+   }
+   @Override
+   public List<ProductVO> getProductList(int start, int finish) {
+      //1 페이지 = 1~ 10
+      //2 페이지 = 11~20
+      //3 페이지 = 21~30
+      //~~
+      System.out.println(start + "" +finish);
+      Map<String, Integer> param = new HashMap<String, Integer>();
+      param.put("start", start);
+      param.put("finish", finish);
+      System.out.println(sqlSession.selectList("product.getProductList",param));
+      return sqlSession.selectList("product.getProductList",param);
+   }
+   @Override
+   public int getProductCount() {
+      return sqlSession.selectOne("product.getProductCount");
+   }
+   @Override
+   public List<ProductVO> getProductList(int start, int finish, String searchType, String keyword) {
+      Map<String, Object> param = new HashMap<String, Object>();
+      param.put("start", start);
+      param.put("finish", finish);
+      param.put("searchType", searchType);
+      param.put("keyword", keyword);
+      
+      return sqlSession.selectList("product.getListOnSearch", param);
+   }
 }
