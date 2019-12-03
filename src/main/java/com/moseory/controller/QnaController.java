53d722e030e0a4a64b122cb9e2de9a96b944ddcc
kimmin <kimmin2301@gmail.com>
2019-12-03 16:33:19 +0900

    .

--- src/main/java/com/moseory/controller/QnaController.java
+++ src/main/java/com/moseory/controller/QnaController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,6 +31,7 @@ import com.moseory.domain.QnaReplyVO;
 import com.moseory.domain.QnaVO;
 import com.moseory.service.QnaService;
 
+import lombok.NonNull;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
